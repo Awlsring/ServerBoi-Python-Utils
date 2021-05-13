aebe35aefef87dfcaef67b4cec8232400bb198ff
Matthew Rawlings <mattcanemail@gmail.com>
2021-05-12 23:39:23 -0700

    Error in zip naming

--- pack.sh
+++ pack.sh
@@ -3,6 +3,6 @@ mkdir -p ./python/lib/python3.8/site-packages
 
 pip3 install serverboi_utils -t ./python/lib/python3.8/site-packages
 
-zip -r serverboi-utils.zip python/
+zip -r serverboi_utils.zip python/
 
 rm -rf python
