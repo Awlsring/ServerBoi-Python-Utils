ad1c4a90bbbb09571a557c799de47ff7a1b59099
Matthew Rawlings <mattcanemail@gmail.com>
2021-05-12 23:59:14 -0700

    Added move to CDK package command

--- pack.sh
+++ pack.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 mkdir -p ./python/lib/python3.8/site-packages
 
-pip3 install serverboi_utils -t ./python/lib/python3.8/site-packages
+pip3 install . -t ./python/lib/python3.8/site-packages
 
 zip -r serverboi_utils.zip python/
 
 rm -rf python
+
+mv serverboi_utils.zip ../ServerlessBoi/lambdas/layers/serverboi_utils/serverboi_utils.zip
