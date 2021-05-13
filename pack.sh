1070a53672de693878b8e0804d7e909cd5c61eba
Matthew Rawlings <mattcanemail@gmail.com>
2021-05-12 23:14:34 -0700

    Added packing script

---
+++ pack.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mkdir -p ./python/lib/python3.8/site-packages
+
+pip3 install serverboi_utils -t ./python/lib/python3.8/site-packages
+
+zip -r serverboi-utils.zip python/
+
+rm -rf python
