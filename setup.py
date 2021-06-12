f60b10e7e5574a512d3455527d58dc0381f7276e
Matthew Rawlings <mattcanemail@gmail.com>
2021-06-12 16:47:23 -0700

    Adding enums as dependencies

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ setup(
     name="ServerBoiUtils",
     version="1.0",
     packages=["serverboi_utils"],
-    install_requires=["python-a2s", "requests"],
+    install_requires=["python-a2s", "requests", "aws-enums", "linode-enums"],
+    dependency_links=[
+        "git+ssh://git@github.com/Awslring/AWS-Enums-Python.git#egg=aws_enums-0.1",
+        "git+ssh://git@github.com/Awslring/Linode-Enums-Python.git#egg=linode_enums-0.1",
+    ],
     include_package_data=True,
 )
