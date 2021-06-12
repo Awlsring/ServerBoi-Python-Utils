63577b3e6a79072bf4e6389ad9916d545e7dd38a
Matthew Rawlings <mattcanemail@gmail.com>
2021-06-12 16:49:44 -0700

    Typo for package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="ServerBoiUtils",
     version="1.0",
     packages=["serverboi_utils"],
-    install_requires=["python-a2s", "requests", "aws-enums", "linode-enums"],
+    install_requires=["python-a2s", "requests", "aws_enums", "linode_enums"],
     dependency_links=[
         "git+ssh://git@github.com/Awslring/AWS-Enums-Python.git#egg=aws_enums-0.1",
         "git+ssh://git@github.com/Awslring/Linode-Enums-Python.git#egg=linode_enums-0.1",
