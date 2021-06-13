c88a030065487f0271d80d46aabb3322f9f83ae0
Matthew Rawlings <mattcanemail@gmail.com>
2021-06-12 17:40:35 -0700

    removing enums

--- setup.py
+++ setup.py
@@ -4,10 +4,6 @@ setup(
     name="ServerBoiUtils",
     version="1.0",
     packages=["serverboi_utils"],
-    install_requires=["python-a2s", "requests", "aws_enums", "linode_enums"],
-    dependency_links=[
-        "git+ssh://git@github.com/Awslring/AWS-Enums-Python.git#egg=aws_enums-0.1",
-        "git+ssh://git@github.com/Awslring/Linode-Enums-Python.git#egg=linode_enums-0.1",
-    ],
+    install_requires=["python-a2s", "requests"],
     include_package_data=True,
 )
